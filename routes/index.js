6789f3a9b177243748fa4be52a48a77cd56c05d1
Lucas Barbula <lucas.barbula@gmail.com>
2016-06-15 16:47:16 -0600

    $@

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,24 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.get('/:id', function (req,res,next){
-  knex('park').where({id:req.params.id}).first().then(function(nature){
-    res.render('detail', {nature:nature})
-  })
-})
+
 
 router.get('/parks', function(req, res, next){
   knex('state').select().then(function(states){
     res.render('parks', {states: states})
   })
 })
+
+router.get('/:id', function (req,res,next){
+  knex('park').where({id:req.params.id}).first().then(function(nature){
+    res.render('detail', {nature:nature})
+  })
+})
+
+router.get('/:id/edit'function(req, res, next){
+  knex('park').where({id: req.params.id}).
+})
+
 router.post('/parks', function(req, res, next){
   knex('park').insert(req.body).then(function(){
     res.redirect('/')
