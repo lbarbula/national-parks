b9fa3d9a1d5924df2770de7d3e153f275a3ab26d
Lucas Barbula <lucas.barbula@gmail.com>
2016-06-15 21:03:21 -0600

    $@

--- routes/index.js
+++ routes/index.js
@@ -17,14 +17,32 @@ router.get('/parks', function(req, res, next){
   })
 })
 
+router.get('/:id/edit', function (req, res, next) {
+  Promise.all([
+    knex('park').where({id: req.params.id}).first(),
+    knex('state').select()
+  ])
+  .then(function (data) {
+    res.render('edit', {park: data[0], states:data[1]})
+  })
+})
+
+router.post('/:id/edit', function(req, res, next){
+  knex('park').where({id: req.params.id}).update(req.body).then(function(){
+    res.redirect('/' + req.params.id)
+  })
+})
+
+router.get('/:id/delete', function (req, res, next) {
+  knex('park').where({id: req.params.id}).del().then(function () {
+    res.redirect('/')
+  })
+})
+
 router.get('/:id', function (req,res,next){
   knex('park').where({id:req.params.id}).first().then(function(nature){
     res.render('detail', {nature:nature})
   })
-})
-
-router.get('/:id/edit'function(req, res, next){
-  knex('park').where({id: req.params.id}).
 })
 
 router.post('/parks', function(req, res, next){
